803b69beb9fdfdb9a27e8db87941d0e82f84056f
Jeff0029 <jeanfrancoisvienneau@gmail.com>
2019-01-26 20:25:41 -0500

    Applied AI roaming / Spawning

---
+++ Project Spooky/Assets/Script/AISpawning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpawning : MonoBehaviour {
+
+    public int m_initialCivilianSpawn = 4;
+    public int m_initialSpiritHunterSpawn = 1;
+    public int m_numberOfSpookedToIncreaseSpiritHunter = 6;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i <= m_initialCivilianSpawn; i++)
+        {
+            Instantiate(Resources.Load("Prefab/Civilian"));
+        }
+
+        for (int i = 0; i <= m_initialSpiritHunterSpawn; i++)
+        {
+            Instantiate(Resources.Load("Prefab/SpiritHunter"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Project Spooky/Assets/Script/Civilian.cs
+++ Project Spooky/Assets/Script/Civilian.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class Civilian : AI {
 
     public Transform m_entrance;
+    public float m_spookIncreaseSpeed = 3;
+    bool m_isSpooked = false;
     // Use this for initialization
     void Start () {
         base.Start();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    protected override Vector3 Spawn()
+    {
+        int index = Random.Range(0, m_roamingLocations.Count - 1);
+        return m_roamingLocations[index].position;
+    }
+
+    // Update is called once per frame
+    override protected void Update () {
+        base.Update();
+    }
+
+    public void GetSpooked()
+    {
+        if (!m_isSpooked)
+        {
+            m_agent.speed += m_spookIncreaseSpeed;
+            m_agent.SetDestination(m_entrance.position);
+        }
+    }
 }
